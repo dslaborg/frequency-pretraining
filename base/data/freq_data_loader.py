fix scaling of sleep data

base/data/freq_data_loader.py
@@ -127,7 +127,7 @@ class DatasetForRandomFrequencies(Dataset):
         )
 
         if self.normalize:
-            x = normalize_epoch(x)
+            x = normalize_epoch(x, "zscore")
 
         # create one-hot encoded labels for the chosen frequency bins
         y = np.zeros(self.n_freqs)

base/data/normalization.py
@@ -1,4 +1,7 @@
 import numpy as np
+from sklearn.preprocessing import RobustScaler
+
+from base.config import Config
 
 
 def zscore(x: np.ndarray, axis: int | list[int]) -> np.ndarray:
@@ -18,13 +21,47 @@ def zscore(x: np.ndarray, axis: int | list[int]) -> np.ndarray:
     return (x - x_mean) / x_std
 
 
-def normalize_epoch(x: np.ndarray) -> np.ndarray:
+def iqr_scale(x: np.ndarray, axis: int | list[int]) -> np.ndarray:
+    """
+    Scale the input data using the interquartile range. The data is first shifted to have a mean of zero and then
+    scaled by the interquartile range. Outliers are clamped to the value specified in the config.
+
+    :param x: input data
+    :param axis: axis to normalize over
+
+    :return: iqr scaled data
+    """
+    if type(axis) is int:
+        axis = [axis]
+    _cfg = Config.get()
+    robust_scaler = RobustScaler()
+    clamp_value = _cfg.data.clamp_value
+
+    shape = np.array(x.shape)
+    axis = np.where(np.array(axis) < 0, np.array(axis) + len(shape), axis)
+    other_axes = np.array([i for i in range(len(shape)) if i not in axis])
+    permute_axes = np.r_[axis, other_axes]
+    x = x.transpose(permute_axes).reshape(np.prod([shape[i] for i in axis]), -1)
+    x = robust_scaler.fit_transform(x)
+    x[x < -clamp_value] = -clamp_value
+    x[x > clamp_value] = clamp_value
+    x = x.reshape(*shape[permute_axes]).transpose(np.argsort(permute_axes))
+    return x
+
+
+def normalize_epoch(x: np.ndarray, norm_type: str) -> np.ndarray:
     """
     Normalize an epoch by z-scoring each epoch.
 
     :param x: input data, datapoints are the last dimension
+    :param norm_type: normalization type, currently only 'zscore' and 'iqr' are supported
 
-    :return: z-score normalized epoch
+    :return: normalized epoch(s)
     """
-    # normalize over last dimension, should be the datapoints, as the shapes are (epoch, channel, datapoints)
-    return zscore(x, axis=-1)
+    if norm_type == "zscore":
+        # normalize over last dimension, should be the datapoints, as the shapes are (epoch, channel, datapoints)
+        return zscore(x, axis=-1)
+    elif norm_type == "iqr":
+        return iqr_scale(x, axis=-1)
+    else:
+        raise ValueError(f"Normalization type {norm_type} is not supported")